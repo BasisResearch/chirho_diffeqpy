754e8136e67a81e2b1a1413a2c9b166f847d2fac
Andy Zane <andy@basis.ai>
2024-08-27 17:07:14 -0400

    adds explicit loading of all julia deps at once.

--- chirho_diffeqpy/__init__.py
+++ chirho_diffeqpy/__init__.py
@@ -3,5 +3,10 @@ import torch
 # Required by DiffEqPy.
 torch.set_default_dtype(torch.float64)
 
+from .load_julia_env import load_julia_env as _load_julia_env
+# This isn't strictly necessary, but loads all julia dependencies at once.
+#  This makes for easier progress tracking during the precompilation process.
+_load_julia_env()
+
 from .handlers import DiffEqPy
 from .internals import ATempParams, PureDynamics
