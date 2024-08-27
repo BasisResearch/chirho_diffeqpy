e7d42cf6d6b46d2d7b717e75a2c367a26cb2d453
Andy Zane <andy@basis.ai>
2024-08-27 18:19:16 -0400

    adds pinned diffeqpy first, so-as to register any dependency pins before those deps are installed.

--- chirho_diffeqpy/load_julia_env.py
+++ chirho_diffeqpy/load_julia_env.py
@@ -3,21 +3,13 @@ from juliacall import Main as jl
 
 
 def load_julia_env():
-    # The diffeqpy import handles just the first three packages, but we also need access to the latter two dependencies.
-    # Use this instead of importing Main directly to ensure the proper environment is loaded.
-    # Note that load_julia_packages also activates a Pkg environment with this stuff in it.
-
-    # Running this first, as it activates the diffeqpy package environment.
-    load_julia_packages(
-        # "DifferentialEquations",  # <-- w/o this for now. See below.
-        "ModelingToolkit",
-        "PythonCall",
-        "Symbolics",
-        "ForwardDiff",
-        "SymbolicUtils"
-    )
+    """
+    Handles julia dependencies and imports. TODO use an explicit Project.toml that also covers diffeqpy's reqs?
+    Use this instead of `from juliacall import Main as jl` to ensure packages are loaded properly.
+    """
 
     # <FIXME Pin to 7.11.0 diffeq>
+
     # Later versions have breaking changes that need addressing...
     pkg = "DifferentialEquations"
     v = "7.11.0"
@@ -25,6 +17,7 @@ def load_julia_env():
 
     # Manually do what the load_julia_packages does, but such that we can specify a version explicitly.
     script = f'''
+    Pkg.activate(\"diffeqpy\", shared=true)
     import Pkg
     try
         import {pkg}
@@ -44,5 +37,19 @@ def load_julia_env():
 
     # ...and pin it so it doesn't update on later Pkg.add calls.
     # </FIXME Pin to 7.11.0 diffeq>
+
+    # The diffeqpy import handles just the first three packages, but we also need access to the latter
+    #  three dependencies.
+    # Note that load_julia_packages also activates a Pkg environment with this stuff in it.
+
+    # Run these afterward, as DifferentialEquations will
+    load_julia_packages(
+        "DifferentialEquations",  # <-- w/o this for now. See below.
+        "ModelingToolkit",
+        "PythonCall",
+        "Symbolics",
+        "ForwardDiff",
+        "SymbolicUtils"
+    )
 
     return jl
