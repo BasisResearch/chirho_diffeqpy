75d0f19428ed2c63fcd166d90209abe56c171904
Andy Zane <andy@basis.ai>
2024-09-19 10:22:13 -0400

    cleans up comments.

--- chirho_diffeqpy/lang_interop/handlers.py
+++ chirho_diffeqpy/lang_interop/handlers.py
@@ -5,7 +5,13 @@ from .ops import convert_julia_to_python, convert_python_to_julia
 
 def callable_from_julia(out_as_first_arg: bool = False, **python_to_julia_kwargs):
     """
-    TODO
+    A backend agnostic decorator that supports wrapping julia inputs to a python function,
+    and then unwrapping the python output to something compatible with julia.
+
+    :param out_as_first_arg: If True, the first argument of the function will be treated as the function output,
+        which will be modified in place.
+    :param python_to_julia_kwargs: Additional keyword arguments to pass to `convert_python_to_julia`. No arguments
+        can be passed to convert_julia_to_python.
     """
 
     def parameterized_decorator_closure(f):
